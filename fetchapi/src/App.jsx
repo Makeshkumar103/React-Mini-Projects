833df14d10fc8a5139e496a3ddfaa3f448f5c3a2
Makeshkumar103 <makeshlg6@gmail.com>
2026-02-05 15:59:03 +0530

    fetching api and displaying content

---
+++ fetchapi/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect, use } from 'react'
+import Form from './Form'
+import List from './List'
+
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+  const [items, setItems] = useState([])
+  const [reqType, setReqType] = useState('users')
+
+  useEffect(() => {
+
+    const fetchItems =async () => {
+      try{
+        const response = await fetch(`${API_URL}${reqType}`)
+        const data = await response.json()
+        setItems(data)
+      }catch(err){
+        console.error(err)
+      }
+    }
+
+    fetchItems()
+  }, [reqType])
+
+
+  return (
+      <div className='App'>
+        <Form reqType={reqType} setReqType={setReqType} />
+        <List items={items} />
+      </div>
+  )
+}
+
+export default App
